2ec7bc2c2ef8b2cf1ea3d3621f829ae2ae88d3e6
Charles Smart <charlie@Charless-MacBook-Pro.local>
2019-07-30 10:41:44 -0400

    Set up three.js renderer

--- src/js/tidal-main.js
+++ src/js/tidal-main.js
@@ -1,16 +1,74 @@
 import loadData from './load-data';
+import * as THREE from 'three';
+
+// Number of rows of boxes;
+const BOX_ROWS = 9;
+const selector = '#tidal-graphic';
+
+let floodingData;
+
+let $wrap = document.querySelector(selector);
+
+let width,
+  height;
+
+let scene,
+  camera,
+  geometries,
+  materials,
+  renderer,
+  particleGroups,
+  light,
+  shaderMaterials;
 
 function init() {
 
-  constructScene();
+
+  loadData(['tidal.json']).then(([d]) => {
+    let maxTotal = d.reduce((sum, e) => sum + parseFloat(e.impactedem33), 0);
+    
+    floodingData = d;
+
+    //console.log((maxTotal / 1000) / BOX_ROWS);
+    constructScene();
+  })
 }
 
 function constructScene() {
-  
+  resize(false);
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.OrthographicCamera(
+    width / 2, 
+    width / 2, 
+    height /2 ,
+    height / 2,
+    1, 
+    1000
+  );
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+
+  renderer.setClearColor('#0e0d12');
+  renderer.setSize(width, height);
+
+  $wrap.appendChild(renderer.domElement);
+
+
 }
 
-function resize() {
+function renderScene() {
 
+}
+
+function resize(render=true) {
+  width = $wrap.getBoundingClientRect().width;
+  height = $wrap.getBoundingClientRect().height;
+
+  if (render) {
+    renderScene();
+  }
 }
 
 export default {
